aededc92309f0eb988ac83764a2724fd819ec69c
iamprashik <iamprashikkoirala@gmail.com>
2026-08-30 13:58:53 -0400

    Day 002: practice const and let variables

---
+++ day-002/README.md
@@ -0,0 +1,19 @@
+# Day 002 - Variables with `const` and `let`
+
+## What I practiced
+
+Today I practiced creating variables in JavaScript using `const` and `let`.
+
+## What I learned
+
+- Use `const` for values that should not be reassigned.
+- Use `let` for values that may change.
+- Variable names should be clear and meaningful.
+
+## Example
+
+```javascript
+const name = "Prashik";
+let completedDays = 1;
+
+completedDays = 2;
---
+++ day-002/script.js
@@ -0,0 +1,16 @@
+
+// const is used for a value that does not change
+const name = "Prashik";
+const learningGoal = "Become a better frontend developer";
+
+// let is used for a value that can change
+let completedDays = 1;
+
+console.log("Name:", name);
+console.log("Goal:", learningGoal);
+console.log("Completed days:", completedDays);
+
+// Updating a let variable
+completedDays = completedDays + 1;
+
+console.log("Updated completed days:", completedDays);
