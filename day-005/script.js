03900d5afc1be5bcd1bdf0c4c959bfaaeb7fb6a8
iamprashik <iamprashikkoirala@gmail.com>
2026-09-02 13:58:53 -0400

    "Day 005: practice JavaScript arithmetic operators"

---
+++ day-005/script.js
@@ -0,0 +1,21 @@
+// Numbers to use for calculations
+const a = 10;
+const b = 3;
+
+// Addition
+console.log("Addition:", a + b);
+
+// Subtraction
+console.log("Subtraction:", a - b);
+
+// Multiplication
+console.log("Multiplication:", a * b);
+
+// Division
+console.log("Division:", a / b);
+
+// Modulus - returns the remainder
+console.log("Remainder:", a % b);
+
+// Exponentiation - raises a to the power of b
+console.log("Power:", a ** b);
