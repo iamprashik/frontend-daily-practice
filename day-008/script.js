ca0697ef32ecc5a7b63c461af6835d6b75487ced
iamprashik <iamprashikkoirala@gmail.com>
2026-09-05 13:07:09 -0400

    Day 008: practice JavaScript template literals

---
+++ day-008/script.js
@@ -0,0 +1,24 @@
+const name = "Prashik";
+const age = 23;
+const goal = "frontend developer";
+
+// Insert variables into a string
+console.log(`My name is ${name}.`);
+
+// Insert multiple variables
+console.log(`I am ${age} years old and my goal is to become a ${goal}.`);
+
+// Use expressions inside a template literal
+const a = 10;
+const b = 5;
+
+console.log(`${a} + ${b} = ${a + b}`);
+
+// Multi-line template literal
+const message = `
+Hello, ${name}!
+Welcome to Day 008 of JavaScript practice.
+Keep learning every day!
+`;
+
+console.log(message);
