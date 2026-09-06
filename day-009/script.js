c32d737aced4acf5b9cca0739e2a0081a0439cc6
iamprashik <iamprashikkoirala@gmail.com>
2026-09-06 12:31:12 -0400

    Day 009: practice JavaScript string length and indexing

---
+++ day-009/script.js
@@ -0,0 +1,21 @@
+const language = "JavaScript";
+
+// Find the length of the string
+console.log("String:", language);
+console.log("Length:", language.length);
+
+// Access characters using indexes
+console.log("First character:", language[0]);
+console.log("Second character:", language[1]);
+console.log("Third character:", language[2]);
+
+// Get the last character
+console.log("Last character:", language[language.length - 1]);
+
+// Another example
+const name = "Prashik";
+
+console.log(`My name is ${name}.`);
+console.log(`My name has ${name.length} characters.`);
+console.log(`The first letter is ${name[0]}.`);
+console.log(`The last letter is ${name[name.length - 1]}.`);
