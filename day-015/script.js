c54d815e0cec0be70f6dba4e65a7bbd94bdc28da
iamprashik <iamprashikkoirala@gmail.com>
2026-09-14 14:07:58 -0400

    "Day 015: practice generating random numbers

---
+++ day-015/script.js
@@ -0,0 +1,19 @@
+// Generate a random decimal between 0 and 1
+const randomDecimal = Math.random();
+
+console.log("Random decimal:", randomDecimal);
+
+// Generate a random whole number from 0 to 9
+const randomNumber = Math.floor(Math.random() * 10);
+
+console.log("Random number from 0 to 9:", randomNumber);
+
+// Generate a random whole number from 1 to 10
+const randomNumber1To10 = Math.floor(Math.random() * 10) + 1;
+
+console.log("Random number from 1 to 10:", randomNumber1To10);
+
+// Generate a random number between 1 and 100
+const randomNumber1To100 = Math.floor(Math.random() * 100) + 1;
+
+console.log("Random number from 1 to 100:", randomNumber1To100);
