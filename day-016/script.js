57829e47bd98eea2799dc4521f8f06e1f10e24b0
iamprashik <iamprashikkoirala@gmail.com>
2026-09-15 14:33:16 -0400

    Day 016: practice even or odd checker

---
+++ day-016/script.js
@@ -0,0 +1,18 @@
+// Number to check
+const number = 17;
+
+// Check if the number is even or odd
+if (number % 2 === 0) {
+  console.log(`${number} is even.`);
+} else {
+  console.log(`${number} is odd.`);
+}
+
+// Another example
+const secondNumber = 24;
+
+if (secondNumber % 2 === 0) {
+  console.log(`${secondNumber} is even.`);
+} else {
+  console.log(`${secondNumber} is odd.`);
+}
