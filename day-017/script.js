b5d1575432cede7269e50f2140063e056a0358d4
iamprashik <iamprashikkoirala@gmail.com>
2026-09-16 13:55:33 -0400

    Day-017: practice positive, negative and zero checker

---
+++ day-017/script.js
@@ -0,0 +1,32 @@
+const number = -10;
+
+if (number > 0) {
+    console.log(`${number} is positive.`);
+}else if (number < 0) {
+    console.log(`${number} is negative.`);
+}
+else {
+    console.log(`${number} is zero.`);
+}
+
+const number1 = 1;
+
+if (number1 > 0) {
+    console.log(`${number1} is positive.`);
+}else if (number1 < 0) {
+    console.log(`${number1} is negative.`);
+}
+else {
+    console.log(`${number1} is zero.`);
+}
+
+const number2 = 0;
+
+if (number2 > 0) {
+    console.log(`${number2} is positive.`);
+}else if (number2 < 0) {
+    console.log(`${number2} is negative.`);
+}
+else {
+    console.log(`The number is zero.`);
+}
