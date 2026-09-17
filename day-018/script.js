560122e7d49f16ffa285ee65abfe2bf9dae1ce82
iamprashik <iamprashikkoirala@gmail.com>
2026-09-17 14:54:26 -0400

    Day 018: practice if else grade checker

---
+++ day-018/script.js
@@ -0,0 +1,13 @@
+const score = 85;
+
+if (score >= 90) {
+    console.log(`Score: ${score} - Grade: A`);
+} else if (score >= 80) {
+    console.log(`Score: ${score} - Grade: B`);
+} else if (score >= 70) {
+    console.log(`Score: ${score} - Grade: C`);
+} else if (score >= 60) {
+    console.log(`Score: ${score} - Grade: D`);
+} else {
+    console.log(`Score: ${score} - Grade: F`);
+}
