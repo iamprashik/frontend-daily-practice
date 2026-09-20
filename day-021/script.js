ac2dada415a2d84d5447477dfdc1e0294ca0bce0
iamprashik <iamprashikkoirala@gmail.com>
2026-09-20 13:29:45 -0400

    switch statements

---
+++ day-021/script.js
@@ -0,0 +1,34 @@
+const day = "Wednesday";
+
+switch (day) {
+    case "Monday":
+        console.log("Today is Monday.");
+        break;
+
+    case "Tuesday":
+        console.log("Today is Tuesday.");
+        break;
+
+    case "Wednesday":
+        console.log("Today is Wednesday.");
+        break;
+
+    case "Thursday":
+        console.log("Today is Thursday.");
+        break;
+
+    case "Friday":
+        console.log("Today is Friday.");
+        break;
+
+    case "Saturday":
+        console.log("Today is Saturday.");
+        break;
+
+    case "Sunday":
+        console.log("Today is Sunday.");
+        break;
+
+    default:
+        console.log("Invalid day.");
+}
