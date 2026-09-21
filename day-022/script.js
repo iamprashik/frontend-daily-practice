31305492385b680d92359b87a02e9e54a597ff2e
iamprashik <iamprashikkoirala@gmail.com>
2026-09-21 14:10:10 -0400

    Day 022: practice for loop counting

---
+++ day-022/script.js
@@ -0,0 +1,14 @@
+// Count from 1 to 5
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+// Count from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log("Number:", i);
+}
+
+// Count backwards from 5 to 1
+for (let i = 5; i >= 1; i--) {
+    console.log("Countdown:", i);
+}
