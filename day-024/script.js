84ea7777c325b98e6cb417b618f4a41c54446177
iamprashik <iamprashikkoirala@gmail.com>
2026-09-23 14:30:49 -0400

    Day 024 - break and continue

---
+++ day-024/script.js
@@ -0,0 +1,19 @@
+// break example
+for (let i = 1; i <= 10; i++) {
+    if (i === 6) {
+        break;
+    }
+
+    console.log("Break:", i);
+}
+
+console.log("----------------");
+
+// continue example
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+
+    console.log("Continue:", i);
+}
