8b8850931c24855ca26a6fdb009d390bae6b90d4
iamprashik <iamprashikkoirala@gmail.com>
2026-09-24 14:19:32 -0400

    day-025: sum calculator

---
+++ day-025/script.js
@@ -0,0 +1,28 @@
+// Sum numbers from 1 to 5
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+    sum += i;
+}
+
+console.log("Sum from 1 to 5:", sum);
+
+
+// Sum numbers from 1 to 10
+let total = 0;
+
+for (let i = 1; i <= 10; i++) {
+    total += i;
+}
+
+console.log("Sum from 1 to 10:", total);
+
+
+// Sum numbers from 1 to 100
+let largeTotal = 0;
+
+for (let i = 1; i <= 100; i++) {
+    largeTotal += i;
+}
+
+console.log("Sum from 1 to 100:", largeTotal);
