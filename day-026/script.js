7ab803b7b8f3eb5aee8d4ca2698c46b662985dd6
iamprashik <iamprashikkoirala@gmail.com>
2026-09-25 14:23:20 -0400

    day-26: multiplication table

---
+++ day-026/script.js
@@ -0,0 +1,15 @@
+// Multiplication table for 5
+const number = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
+
+console.log("----------------");
+
+// Multiplication table for 7
+const number2 = 7;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number2} x ${i} = ${number2 * i}`);
+}
