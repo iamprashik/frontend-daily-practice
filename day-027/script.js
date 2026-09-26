1d57fd055108ce927b6cf68313c5e30d525b4dac
iamprashik <iamprashikkoirala@gmail.com>
2026-09-26 13:27:32 -0400

    day-027: practice function declaration

---
+++ day-027/script.js
@@ -0,0 +1,25 @@
+// Create a simple function
+function greet() {
+    console.log("Hello, Prashik!");
+}
+
+// Call the function
+greet();
+
+
+// Create another function
+function sayWelcome() {
+    console.log("Welcome to JavaScript practice!");
+}
+
+sayWelcome();
+
+
+// A function can be called multiple times
+function showMessage() {
+    console.log("Keep learning!");
+}
+
+showMessage();
+showMessage();
+showMessage();
